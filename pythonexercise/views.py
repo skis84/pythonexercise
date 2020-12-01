fef980e07513f2efff87e32637c745ee35cecc7c
Sanna Luukkonen <39089796+skis84@users.noreply.github.com>
2020-12-01 09:31:09 +0200

    Update views.py

--- pythonexercise/views.py
+++ pythonexercise/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 
-
+import os 
+import pytest
 def home(request):
     return HttpResponse(
         'Please go to <a href="/api/company/1">api/company/<business_id></>')
