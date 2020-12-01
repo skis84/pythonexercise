8a43d67319de4920612d07c9ca67a102a4fd904c
Sanna Luukkonen <39089796+skis84@users.noreply.github.com>
2020-12-01 10:29:48 +0200

    Update urls.py

--- pythonexercise/urls.py
+++ pythonexercise/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import home
+import os
 
 urlpatterns = [
     path('', home, name="home"),
